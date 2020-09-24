f84bfe603a13b514af9c71f059a5b74c8caf90d2
Tom Withers <tomtucka@gmail.com>
2020-09-24 21:37:29 +0000

    Setup precheck

--- circleci/provider_test.go
+++ circleci/provider_test.go
@@ -1,10 +1,20 @@
 package circleci
 
 import (
+	"os"
 	"testing"
 
+	"github.com/TomTucka/go-circleci"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/stretchr/testify/require"
 )
+
+var providerFactories = map[string]func() (*schema.Provider, error){
+	"circleci": func() (*schema.Provider, error) {
+		return Provider(), nil
+	},
+}
 
 func TestProvider_HasChildResources(t *testing.T) {
 	expectedResources := []string{
@@ -41,5 +51,36 @@ func TestProvider_SchemaIsValid(t *testing.T) {
 		require.NotEmpty(t, test.token, "A property in the schema cannot have a nil value")
 		require.NotNil(t, test.organization, "A property in the schema cannot have a nil value")
 		require.NotNil(t, test.organization, "A property in the schema cannot have a nil value")
+	}
+}
+
+var testClient *circleci.Client
+
+func importStep(name string, ignore ...string) resource.TestStep {
+	step := resource.TestStep{
+		ResourceName:      name,
+		ImportState:       true,
+		ImportStateVerify: true,
+	}
+
+	if len(ignore) > 0 {
+		step.ImportStateVerifyIgnore = ignore
+	}
+
+	return step
+}
+
+func preCheck(t *testing.T) {
+	variables := []string{
+		"CIRCLECI_TOKEN",
+		"CIRCLECI_ORGANIZATION",
+		"CIRCLECI_VCS_TYPE",
+	}
+
+	for _, variable := range variables {
+		value := os.Getenv(variable)
+		if value == "" {
+			t.Fatalf("`%s` must be set for acceptance tests!", variable)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/TomTucka/terraform-provider-circleci
 go 1.14
 
 require (
-	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
 	github.com/TomTucka/go-circleci v0.4.0
-	github.com/stretchr/testify v1.3.0
+	github.com/hashicorp/terraform v0.13.3
+	github.com/hashicorp/terraform-plugin-sdk v1.16.0
+	github.com/hashicorp/terraform-plugin-sdk/v2 v2.0.0-rc.2
+	github.com/stretchr/testify v1.5.1
 )
